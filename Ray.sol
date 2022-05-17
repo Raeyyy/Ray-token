7353fa322424978d234a4e0f03f3e0f909018aee
Raeyyy <101048579+Raeyyy@users.noreply.github.com>
2022-05-17 14:34:44 +0100

    Create Ray.sol

---
+++ Ray.sol
@@ -0,0 +1,13 @@
+ Ray is ERC20 {
+    constructor(uint256 initialsupply) ERC20( "Ray", "R")  {
+        _mint (msg.sender, initialsupply);
+    }
+    function decimals() public view virtual override returns (uint8){
+        re//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contractturn 8;
+    }
+}
